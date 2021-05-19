f76318118848abb7f0eb1a29c028b830eed76d4d
Albert Xu <tooflesswulf0325@gmail.com>
2021-05-19 01:22:17 -0400

    Replaced all the tabs with spaces

--- Source/HoudiniEngineRuntime/Public/HoudiniBlueprintFunctions.h
+++ Source/HoudiniEngineRuntime/Public/HoudiniBlueprintFunctions.h
@@ -29,14 +29,14 @@ class HOUDINIENGINERUNTIME_API UHoudiniBlueprintFunctions : public UBlueprintFun
 
     // Getters/Accessors
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static int GetHAssetFrame(AHoudiniAssetActor* HoudiniAssetActor) { return GetHInt(HoudiniAssetActor, "frame"); }
+    static int GetHAssetFrame(AHoudiniAssetActor* HoudiniAssetActor) { return GetHInt(HoudiniAssetActor, "frame"); }
 
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static bool GetHAssetExtrToggle(AHoudiniAssetActor* HoudiniAssetActor) { return GetHBool(HoudiniAssetActor, "extrude"); }
+    static bool GetHAssetExtrToggle(AHoudiniAssetActor* HoudiniAssetActor) { return GetHBool(HoudiniAssetActor, "extrude"); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static float GetHAssetExtrTemp(AHoudiniAssetActor* HoudiniAssetActor) { return GetHFloat(HoudiniAssetActor, "extr_temp"); }
+    static float GetHAssetExtrTemp(AHoudiniAssetActor* HoudiniAssetActor) { return GetHFloat(HoudiniAssetActor, "extr_temp"); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static float GetHAssetExtrVel(AHoudiniAssetActor* HoudiniAssetActor) { return GetHFloat(HoudiniAssetActor, "extr_vel"); }
+    static float GetHAssetExtrVel(AHoudiniAssetActor* HoudiniAssetActor) { return GetHFloat(HoudiniAssetActor, "extr_vel"); }
 
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
     static void GetHAssetExtrPos(AHoudiniAssetActor* HoudiniAssetActor, FVector& ret) { return GetHVector(HoudiniAssetActor, "extr_pos", ret); }
@@ -46,7 +46,7 @@ class HOUDINIENGINERUNTIME_API UHoudiniBlueprintFunctions : public UBlueprintFun
     static void GetHAssetExtrTransform(AHoudiniAssetActor* HoudiniAssetActor, FTransform& ret);
 
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static bool GetHAssetRemoverToggle(AHoudiniAssetActor* HoudiniAssetActor) { return GetHBool(HoudiniAssetActor, "remove"); }
+    static bool GetHAssetRemoverToggle(AHoudiniAssetActor* HoudiniAssetActor) { return GetHBool(HoudiniAssetActor, "remove"); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
     static void GetHAssetRemoverPos(AHoudiniAssetActor* HoudiniAssetActor, FVector& ret) { return GetHVector(HoudiniAssetActor, "remr_pos", ret); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
@@ -59,36 +59,35 @@ class HOUDINIENGINERUNTIME_API UHoudiniBlueprintFunctions : public UBlueprintFun
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
     static void HAssetAdvanceFrame(AHoudiniAssetActor* HoudiniAssetActor, int num_frames);
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetToggleExtrude(AHoudiniAssetActor* HoudiniAssetActor);
     // UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	// static void HAssetChangePos(AHoudiniAssetActor* HoudiniAssetActor, float dx, float dy, float dz);
+    // static void HAssetChangePos(AHoudiniAssetActor* HoudiniAssetActor, float dx, float dy, float dz);
 
     // Setters
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetFrame(AHoudiniAssetActor* HoudiniAssetActor, int frame) { SetHInt(HoudiniAssetActor, "frame", frame); }
+    static void HAssetSetFrame(AHoudiniAssetActor* HoudiniAssetActor, int frame) { SetHInt(HoudiniAssetActor, "frame", frame); }
 
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetExtrude(AHoudiniAssetActor* HoudiniAssetActor, bool state) { SetHBool(HoudiniAssetActor, "extrude", state); }
+    static void HAssetSetExtrude(AHoudiniAssetActor* HoudiniAssetActor, bool state) { SetHBool(HoudiniAssetActor, "extrude", state); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetExtrTemp(AHoudiniAssetActor* HoudiniAssetActor, float temp) { SetHFloat(HoudiniAssetActor, "extr_temp", temp); }
+    static void HAssetSetExtrTemp(AHoudiniAssetActor* HoudiniAssetActor, float temp) { SetHFloat(HoudiniAssetActor, "extr_temp", temp); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetExtrVel(AHoudiniAssetActor* HoudiniAssetActor, float vel) { SetHFloat(HoudiniAssetActor, "extr_vel", vel); }
+    static void HAssetSetExtrVel(AHoudiniAssetActor* HoudiniAssetActor, float vel) { SetHFloat(HoudiniAssetActor, "extr_vel", vel); }
 
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetExtrPos(AHoudiniAssetActor* HoudiniAssetActor, const FVector& pos) { SetHVector(HoudiniAssetActor, "extr_pos", pos); }
-	UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetExtrRot(AHoudiniAssetActor* HoudiniAssetActor, const FQuat& quat) { SetHQuat(HoudiniAssetActor, "extr_rot", quat); }
+    static void HAssetSetExtrPos(AHoudiniAssetActor* HoudiniAssetActor, const FVector& pos) { SetHVector(HoudiniAssetActor, "extr_pos", pos); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetExtrTransform(AHoudiniAssetActor* HoudiniAssetActor, const FTransform& trans);
+    static void HAssetSetExtrRot(AHoudiniAssetActor* HoudiniAssetActor, const FQuat& quat) { SetHQuat(HoudiniAssetActor, "extr_rot", quat); }
+    UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
+    static void HAssetSetExtrTransform(AHoudiniAssetActor* HoudiniAssetActor, const FTransform& trans);
 
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetRemove(AHoudiniAssetActor* HoudiniAssetActor, bool state) { SetHBool(HoudiniAssetActor, "remove", state); }
+    static void HAssetSetRemove(AHoudiniAssetActor* HoudiniAssetActor, bool state) { SetHBool(HoudiniAssetActor, "remove", state); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetRemoverPos(AHoudiniAssetActor* HoudiniAssetActor, const FVector& pos) { SetHVector(HoudiniAssetActor, "remr_pos", pos); }
-	UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetRemoverRot(AHoudiniAssetActor* HoudiniAssetActor, const FQuat& quat) { SetHQuat(HoudiniAssetActor, "remr_rot", quat); }
+    static void HAssetSetRemoverPos(AHoudiniAssetActor* HoudiniAssetActor, const FVector& pos) { SetHVector(HoudiniAssetActor, "remr_pos", pos); }
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
-	static void HAssetSetRemoverTransform(AHoudiniAssetActor* HoudiniAssetActor, const FTransform& trans);
+    static void HAssetSetRemoverRot(AHoudiniAssetActor* HoudiniAssetActor, const FQuat& quat) { SetHQuat(HoudiniAssetActor, "remr_rot", quat); }
+    UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
+    static void HAssetSetRemoverTransform(AHoudiniAssetActor* HoudiniAssetActor, const FTransform& trans);
 
     // HAssetCook needs to be called to ask Houdini to cook the asset.
     UFUNCTION(BlueprintCallable, Category="Houdini FDM nozzle")
